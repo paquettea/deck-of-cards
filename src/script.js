42a1f93ab72537810a9eb76415f0d4baf2652ee8
Philippe Adam <philippeadamweb.com>
2020-03-25 17:33:17 -0400

    player now a vue component

--- src/script.js
+++ src/script.js
@@ -3,8 +3,7 @@ let transform = prefix('transform');
 let translate = Deck.translate;
 let deck = Deck(false);
 let onTouch = () => {
-    //this === card;
-    debugger;
+  //this === card;
 };
 deck.cards.forEach( (card, index) => {
     card.enableDragging();
@@ -16,19 +15,20 @@ deck.mount(document.getElementById("container"));
 deck.intro();
 
 
-
-class Player {
-  constructor(name, stack){
-    this.name = name;
-    this.stack = stack;
-  }
-}
-
+const playerStatus = {
+  FOLDED: 'folded',
+  IN_HAND: 'in-hand',
+  ALL_IN: 'all-in'
+};
 
 
 const table = new Vue({
   el: '#table',
   data: {
+    minBuyIn: 5,
+    maxBuyIm: 20,
+    smallBlind: 0.25,
+    bigBlind: 0.25,
     players: []
   },
   methods: {
@@ -37,10 +37,52 @@ const table = new Vue({
     },
     removePlayer(index) {
       this.players.splice(index, 1);
+    },
+    startHand() {
+      this.$refs.playersRef.forEach(player => {
+        if (player.stack > 0) {
+          player.play();
+        }
+      })
     }
   }
 });
 
+Vue.component('player', {
+  props: ['name', 'startingStack'],
+  data() {
+    return {
+      status: playerStatus.WAITING,
+      stack: this.startingStack
+    }
+  },
+  template: `
+    <div>
+      <div class="player">
+        {{ name }} - {{ stack }}$ 
+      </div>
+      <div class="actions">
+        <button class="btn-action" @click="fold()" :disabled="status != 'in-hand'">Fold</button>
+        <button class="btn-action" @click="bet(2)" :disabled="status != 'in-hand'">Bet 2$</button>
+      </div>
+    </div>  
+  `,
+  methods: {
+    play() {
+      this.status = playerStatus.IN_HAND;
+    },
+    fold() {
+      this.status = playerStatus.FOLDED;
+    },
+    allIn() {
+      this.status = playerStatus.ALL_IN;
+    },
+    bet(amount) {
+      const betAmount = amount < this.stack ? amount : this.stack;
+      this.stack -= betAmount;
+    }
+  }
+});
 
 
 const playersForm = new Vue({
@@ -51,8 +93,11 @@ const playersForm = new Vue({
   },
   methods: {
     onSubmit() {
-      const player = new Player(this.name, this.stack);
-      table.addPlayer(player);
+      table.addPlayer({
+        name: this.name,
+        stack: this.stack,
+        status: playerStatus.WAITING
+      });
 
       this.name = '';
       this.stack = '';
