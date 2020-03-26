721a748b0e8f3185c63f4a19aebd20832549daff
Philippe Adam <philippeadamweb.com>
2020-03-25 20:19:17 -0400

    hand positions

--- src/script.js
+++ src/script.js
@@ -21,14 +21,22 @@ const playerStatus = {
   ALL_IN: 'all-in'
 };
 
-
-const table = new Vue({
-  el: '#table',
-  data: {
+const store = new Vuex.Store({
+  state: {
     minBuyIn: 5,
     maxBuyIm: 20,
     smallBlind: 0.25,
     bigBlind: 0.25,
+    players: [],
+    nextToAct: null,
+    foldedPlayers: []
+  }
+});
+
+// Table 
+const table = new Vue({
+  el: '#table',
+  data: {
     players: []
   },
   methods: {
@@ -39,17 +47,33 @@ const table = new Vue({
       this.players.splice(index, 1);
     },
     startHand() {
+      this.setNextToAct();
       this.$refs.playersRef.forEach(player => {
         if (player.stack > 0) {
           player.play();
         }
       })
+    },
+    setNextToAct() {
+      store.state.nextToAct = this.getNextPosition();
+    },
+    getNextPosition() {
+      const isNotSet = store.state.nextToAct === null;
+      const isLastOfArray = store.state.nextToAct + 1 === this.players.length;
+      return isNotSet || isLastOfArray ? 0 : store.state.nextToAct + 1;
+    }
+  },
+  computed: {
+    canStartHand() {
+      return this.players.length > 1;
     }
   }
 });
 
+
+// Player component
 Vue.component('player', {
-  props: ['name', 'startingStack'],
+  props: ['name', 'startingStack', 'position'],
   data() {
     return {
       status: playerStatus.WAITING,
@@ -57,14 +81,16 @@ Vue.component('player', {
     }
   },
   template: `
-    <div>
-      <div class="player">
-        {{ name }} - {{ stack }}$ 
+    <div class="player">
+      <div>
+        {{ name }} - {{ stack }}$
+        <b class="status" v-if="status == 'folded'">FOLDED</b>
       </div>
       <div class="actions">
-        <button class="btn-action" @click="fold()" :disabled="status != 'in-hand'">Fold</button>
-        <button class="btn-action" @click="bet(2)" :disabled="status != 'in-hand'">Bet 2$</button>
+        <button class="btn-action" @click="fold()" :disabled="!isInHand || !isNextToAct">Fold</button>
+        <button class="btn-action" @click="bet(2)" :disabled="!isInHand || !isNextToAct">Bet 2$</button>
       </div>
+      <div v-if="isNextToAct" class="next-to-act"></div>
     </div>  
   `,
   methods: {
@@ -73,18 +99,37 @@ Vue.component('player', {
     },
     fold() {
       this.status = playerStatus.FOLDED;
+      store.state.foldedPlayers.push(this.position);
+      this.$emit('played', 'fold');
     },
     allIn() {
       this.status = playerStatus.ALL_IN;
+      this.$emit('played', 'all in');
     },
     bet(amount) {
-      const betAmount = amount < this.stack ? amount : this.stack;
+      const isAllIn = amount >= this.stack;
+      const betAmount = isAllIn ? this.stack : amount;
+
+      if (isAllIn) {
+        this.allIn();
+      }
+
       this.stack -= betAmount;
+      this.$emit('played', 'bet');
+    }
+  },
+  computed: {
+    isNextToAct() {
+      return this.position === store.state.nextToAct;
+    }, 
+    isInHand() {
+      return this.status != playerStatus.FOLDED;
     }
   }
 });
 
 
+// Create players form
 const playersForm = new Vue({
   el: '#players-form',
   data: {
@@ -95,8 +140,7 @@ const playersForm = new Vue({
     onSubmit() {
       table.addPlayer({
         name: this.name,
-        stack: this.stack,
-        status: playerStatus.WAITING
+        stack: this.stack
       });
 
       this.name = '';
